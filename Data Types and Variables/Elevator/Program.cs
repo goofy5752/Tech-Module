18f616a15fc12138dbb148d3980c1d9db11cad27
malloy5752 <m.asenov5752@gmail.com>
2019-05-31 19:24:16 +0300

    new task

---
+++ Data Types and Variables/Elevator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPPL = int.Parse(Console.ReadLine());
+            int capacityOfElevator = int.Parse(Console.ReadLine());
+
+            if (numberOfPPL <= capacityOfElevator)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                int courses = (int)Math.Ceiling((double)numberOfPPL / capacityOfElevator);
+                Console.WriteLine(courses);
+            }
+        }
+    }
+}
