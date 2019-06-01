9ad250996f9d0fddad747012d904380fb5b0603e
malloy5752 <m.asenov5752@gmail.com>
2019-06-01 03:42:28 +0300

    Task Added

---
+++ Data Types and Variables/SumOfChars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+
+            for (int i = 0; i < inputNumber; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+
+                totalSum += letter;
+            }
+
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
